63fc6e922d906cc90729d50573c56e61b5ed592e
digikar <digikar@proton.me>
2026-03-31 21:52:41 +0200

    [fix] better handle msys2 vs non-msys2 environments

--- lisp/ql-https.lisp
+++ lisp/ql-https.lisp
@@ -1,14 +1,35 @@
 (in-package :ql-https)
 
 (defun ensure-ssl-tools ()
-  (loop :for program :in '("curl" "openssl" "tar" "git")
-        :do (uiop:run-program (list "which" program) :error t)))
+  (handler-case
+      (loop :for program :in '("curl" "openssl" "tar" "git")
+            :do (uiop:run-program (list "which" program) :error-output t)
+            :finally (return t))
+    (error ()
+      (format *error-output* "Make sure curl, openssl, tar, git are installed and available.~%If you are windows, use msys2 mingw!~%")
+      nil)))
+
+(defvar *cygpath-p*)
 
 (defvar *quicklisp-sexp-url* "https://beta.quicklisp.org/client/quicklisp.sexp")
 
 (defvar *ql-https-url* "https://github.com/rudolfochrist/ql-https")
 
-(defvar *ql-https-dir* (uiop:strcat (uiop:getenv "HOME") "/common-lisp/ql-https"))
+(defvar *ql-https-dir*)
+
+(defun setup-paths ()
+  (setf *cygpath-p*
+        (when (member :windows cl:*features*)
+          (ignore-errors
+           (zerop (nth-value 2 (uiop:run-program (list "which" "cygpath")
+                                                 :ignore-error-status t))))))
+  (setf *ql-https-dir* (uiop:native-namestring (merge-pathnames "common-lisp/ql-https/" (user-homedir-pathname))))
+  (setf *ql-https-dir*
+        (if *cygpath-p*
+            (uiop:run-program (list "cygpath" *ql-https-dir*) :output '(:string :stripped t))
+            *ql-https-dir*)))
+
+
 
 
 (defun install-quicklisp-client (&optional (dir ql-setup:*quicklisp-home*))
@@ -28,8 +49,14 @@
                (uiop:strcat "https" (subseq client-url 4))))
          (expected-client-sha256 (getf client-tar :sha256))
          (client-tar-path
-           (namestring
-            (merge-pathnames "quicklisp.tar" dir))))
+           (uiop:native-namestring
+            (merge-pathnames "quicklisp.tar" dir)))
+         (client-tar-path
+           (if *cygpath-p*
+               (uiop:run-program (format nil "cygpath ~A" client-tar-path)
+                                 :output '(:string :stripped t)
+                                 :error-output t)
+               client-tar-path)))
 
     (format t "Downloading quicklisp client...~%")
     (uiop:run-program (format nil "curl -s '~A' -o ~A"
@@ -82,12 +109,22 @@
 
 (defun install-quicklisp (&optional (dir ql-setup:*quicklisp-home*))
 
-  (ensure-ssl-tools)
+  (unless (ensure-ssl-tools)
+    (format *error-output* "~%Cannot install quicklisp.~%")
+    (return-from install-quicklisp nil))
+
   (ensure-directories-exist dir)
   (install-quicklisp-client dir)
   (install-ql-https)
 
-  (load (uiop:strcat *ql-https-dir* "/ql-setup.lisp"))
+  (let ((ql-setup-path (namestring (merge-pathnames "ql-setup.lisp" *ql-https-dir*))))
+    (setf ql-setup-path
+          (if *cygpath-p*
+              (uiop:run-program (list "cygpath" "-w" ql-setup-path)
+                                :output '(:string :stripped t)
+                                :error-output t)
+              ql-setup-path))
+    (load ql-setup-path))
   (assert (equalp ql-http:*fetch-scheme-functions*
                   '(("http" . ql-https:fetcher)
                     ("https" . ql-https:fetcher))))
@@ -102,6 +139,7 @@
 
 (defun ensure-quicklisp
     (&optional (quicklisp-home ql-setup:*quicklisp-home*))
+  (setup-paths)
   (let ((ql:*quicklisp-home* quicklisp-home)
         (dir quicklisp-home))
     (if (quicklisp-installed-p dir)
